aa887093e3416800be0a214ec7240926593ad550
Archit Dadwal <83974549+architdadwal@users.noreply.github.com>
2024-03-01 10:31:07 +0530

    00
    0000

---
+++ app/pages/api/login.js
@@ -0,0 +1,45 @@
+import Cookies from "cookies";
+import clientPromise from "../../lib/mongodb";
+const { createHash } = require("node:crypto");
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const username = req.body["username"];
+    const guess = req.body["password"];
+    const client = await clientPromise;
+
+    try {
+      const db = client.db("Users");
+      const users = await db
+        .collection("Profiles")
+        .find({ Username: username })
+        .toArray();
+      console.log("1", username);
+
+      if (users.length === 0) {
+        res.redirect("/login?msg=Incorrect username or password");
+        return;
+      }
+
+      const user = users[0];
+      const guess_hash = createHash("sha256").update(guess).digest("hex");
+
+      if (guess_hash === user.Password) {
+        // Set the cookie
+        const cookies = new Cookies(req, res);
+        cookies.set("username", username);
+
+        // Redirect to the home page with the subdomain in the URL
+        const subdomain = username.toLowerCase();
+        res.redirect(`http://${subdomain}.localhost:3000/`);
+      } else {
+        res.redirect("/login?msg=Incorrect username or password");
+      }
+    } catch (error) {
+      console.error("Error during login:", error);
+      res.status(500).send("Internal Server Error");
+    }
+  } else {
+    res.redirect("/");
+  }
+}
---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true, // Enable the experimental `app` directory
+  },
+};
+
+module.exports = nextConfig;
